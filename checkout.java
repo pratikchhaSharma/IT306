e6301a1156d1af5baabad6488c23111e131f672b
guilcong <45617871+guilcong@users.noreply.github.com>
2019-04-22 02:49:29 -0400

    Update checkout.java

--- checkout.java
+++ checkout.java
@@ -3,7 +3,9 @@ package IT306Project;
 public class checkout 
 {
 	item[] cart = new item[item.MAX_NUM_ITEMS];
+	item[] items = new item[item.MAX_NUM_ITEMS];
 	String path = "./src/IT306Project/inventory.txt";
+	items = readFromFile(path, items)
 	String menu ="1- Add to cart\n2-Remove from cart\n3-Search\n4-Check holds\n5-Checkout.";
 	int option = 0;
 	do{
@@ -26,6 +28,38 @@ public class checkout
 			checkout(cart, path)
 		}
 	}while(true);
+}
+
+public static item[] readFromFile(String path, item[] items)
+{
+	try{
+		String line =null; 
+		Scanner scan =new Scanner(new FileInputStream(new File(path)));  
+		Scanner scan2 =null; 
+		int counter=0; 
+		while(scan.hasNextLine()){
+			line = scan.nextLine();
+			scan2 = new Scanner(line);
+			scan2.useDelimiter(","); 
+			//tokenizing the line variable
+			//how can we define what kind of item it is?
+			String name = scan2.next();
+			String genre = scan2.next();
+			String publisher = scan2.next();
+			String publicationDate = scan2.next();
+			double price = Double.parseDouble(scan2.next().trim());			
+			Item s = new Item(name, genre, publisher, publicationDate, price);
+			items[counter++]=s;
+			System.out.println(line);
+		}
+	}
+	catch(FileNotFoundException e){
+		e.printStackTrace(); 
+	}
+	catch( IOException e){
+		e.printStackTrace(); 
+	}
+	return items;
 }
 
 /*The addToCart method will allow a user to add an item to their cart. The cart will be sorted by name (A to Z) or price (Low to High) 
