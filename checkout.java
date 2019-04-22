e3767f86fd236a2efc75f339b309795c598f56b2
guilcong <45617871+guilcong@users.noreply.github.com>
2019-04-22 00:22:41 -0400

    Update checkout.java
    First draft, need to connect to word document

--- checkout.java
+++ checkout.java
@@ -2,6 +2,46 @@ package IT306Project;
 
 public class checkout 
 {
-	
+	String menu ="1- Add to cart\n2-Remove from cart\n3-Search\n4-Check holds\n5-Checkout.";
+	int option = 0;
+	do{
+		option = Integer.parseInt(JOptionPane.showInputDialog(menu));
+		switch(option){
+		case 1:
+			//Add item to cart
+			addToCart(item, cart)
+		case 2:
+			//Remove item from cart
+			removeFromCart(item, cart)
+		case 3:
+			//Search for an item
+			search(name)
+		case 4:
+			//Check item for holds
+			checkHold(item)
+		case 5:
+			//Checkout
+			checkout(cart)
+		}
+	}while(true);
+}
 
+public static void addToCart(Object Item, Cart Cart){
+	
+}
+
+public static void removeFromCart(Object Item, Cart Cart){
+	
+}
+
+public static void search(){
+
+}
+
+public static void checkHold(Inventory[] Items){
+	
+}
+
+public string checkout(Cart Cart){
+	return "Fill";
 }
