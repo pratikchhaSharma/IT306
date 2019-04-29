5e5a0614bdddef9df209b43adf3301041e8528e0
qquach1 <41005279+qquach1@users.noreply.github.com>
2019-04-29 17:16:56 -0400

    Update journal.java

--- journal.java
+++ journal.java
@@ -2,27 +2,32 @@ package IT306Project;
 
 public class journal extends item
 {
-
+	private int volume; 
+	// Constructor that calls the super variables from parent class Item 
+	//instantiates the variable volume
 	public journal(String name, String author, String genre, String publisher, String publicationDate, double price)
 	{
 		super(name, author, genre, publisher, publicationDate, price);
 		this.volume=volume;
 	}
-	private int volume; 
 	
+	// mutator used to set the volume 
 	public void setVolume(int volume)
 	{
 		this.volume=volume;
 	}
+	// accessor to return the journal volume 
 	public int getVolume()
 	{
 		return this.volume;
 	}
-	
+	//method to calculate the total price of a journal
+	//Derived from an abstract method in the parent class item
 	public double calculateTotal()
 	{
 		return super.getPrice() + (super.getPrice() * checkout.tax);
 	}
+	//method to calculate the price without tax 
 	public double calculateSubTotal()
 	{
 		return super.getPrice();
