e0a619d8d5198474af9b9f9260a77dd0bf4dc493
qquach1 <41005279+qquach1@users.noreply.github.com>
2019-04-29 17:03:05 -0400

    Update book.java

--- book.java
+++ book.java
@@ -4,26 +4,32 @@ public class book extends item
 {
 	private String isbn;
 	
+	//Creates a constructor that calls the parents variables from parent class Item 
+	//instanticates the isbn variable 
 	public book(String name, String author, String genre, String publisher, String publicationDate, double price)
 	{
 	super(name, author, genre, publisher, publicationDate, price);
 	this.isbn=isbn;
 	}
+	
+	//mutator for setting the isbn
 	public void setIsbn (String isbn)
 	{
 		this.isbn=isbn;
 	}
+	//accessor for returning the isbn 
 	public String getIsbn()
 	{
 		return this.isbn;
 	}
 	
-
+	//method to calculate the price of a book. Derived from an abstract method from the parent class 
 	public double calculateTotal()
 	{
 		return super.getPrice() + (super.getPrice() * checkout.tax);
 	}
 	
+	//method to calculate the sub total which doesn't include tax 
 	public double calculateSubTotal()
 	{
 		return super.getPrice();
